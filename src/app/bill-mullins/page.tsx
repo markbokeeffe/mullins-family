df1e490155a346b3aa9e9fb4647a1e39f0269ddd
markbokeeffe <mark.okeeffe@gmail.com>
2024-09-19 14:15:13 +0100

    Bill Mullins

--- src/app/bill-mullins/page.tsx
+++ src/app/bill-mullins/page.tsx
@@ -12,35 +12,49 @@ export default function Page() {
             width={300}
             height={24}
           /> 
-          <p><span style={{ color: '#052003' }} className="font-bold">The Maxey Family was a well-known family that lived on what was then known as Johnson Street (now Gladstone Street) in Clonmel in the mid-nineteenth century.</span></p>
-          <p>William Maxey was a publican during the 1840s and 1850s who had his premises at 11 Johnson Street and he was married to Bridget Kelly.  William was quite prominent in Clonmel and was listed as a voter for the town.  He also held the occuptation of cooper, which his sons would follow him into.</p>
+          <p><span style={{ color: '#052003' }} className="font-bold">Bill Mullins was born in Clonmel in Co. Tipperary on 15 September 1911 to John and Johanna Mullins n&#233;e Donegan.</span></p>
+         
           <Image
-            src="/wmmaxey_business.png"
-            alt="William Maxey business"
-            caption="William Maxey&#39;s business listed in Griffith&#39;s Valuation"
-            className="mt-4"
+            src="/billMullinsBurma.png"
+            alt="Bill Mullins in Burma"
+            caption="Bill Mullinson the left of the group in Burma"
             float="right"
             width={300}
             height={24}
           /> 
-          <p>William was born around 1799 and he married Bridget Kelly some time before 1833.  Together, they had at least five children:</p>
-          <ul>
-            <li>- William (1833 -)</li>
-            <li>- James (1835 -)</li>
-            <li>- Timothy William (1837 - 1896)</li>
-            <li>- Joseph (1843 -)</li>
-            <li>- Mary Anne (1844 - 1895)</li>
-          </ul>
-          <p>Some time between the 1850s and the mid-1860s, William Maxey fell on hard times.  When his son, Joseph, was imprisoned in 1866-67, pleas for his release were made on the basis that his family had fallen into destitution, including his father William, who was dependent upon him.</p>
+          <p>At a young age, Bill emigrated to London and followed his father&#39;s footsteps into the British Army.  By the time that the Second World War broke out in 1939, Bill was already an experienced soldier.  He was in the 2nd Battalion of the Royal Norfolk Regiment.</p>
           <Image
-            src="/williamMaxeyPlea.png"
-            alt="William Maxey Plea"
-            caption="A plea for the release from prison of William&#39;s son Joseph, citing William&#39;s age and dependence upon him."
+            src="/billMullinsCamp.png"
+            alt="Bill Mullins in his camp during the war"
+            caption="Bill Mullins in his camp during the war"
+            width={300}
+            height={24}
+          /> 
+          
+          <p>During the war years, Bill spent time initially in France.  He was present during the evacuation of Dunkirk in 1940.  Later, he was stationed in Burma and in India.</p>  
+          <p>In the spring and summer of 1944, his battalion took part in one of the most fierce battles of the entire war: the Battle of Kohima.  In this battle, the Allies forced Japan to abandon much of what it had captured in India.  This was a turning point for the Allie and it changed the course of the war in the Pacific.</p>
+
+          <p>Following the end of the war, Bill remained as a soldier.  He took part in boxing competitions in the army and won a number of prizes.</p>
+          <Image
+            src="/billMullinsGroup.png"
+            alt="Bill Mullins group"
+            caption="Bill Mullins pictured in the centre of this group"
+            className="mt-4"
+            float="right"
+            width={400}
+            height={24}
+          />
+          
+          <p>In his 80s, in the mid-1990s, Bill left London and returned to live in Clonmel.  He died there at the age of 86 on 12 June 1998.</p>
+
+          <Image
+            src="/billMullinsOld.png"
+            alt="Bill Mullins in old age in Clonmel in the late 1990s"
+            caption="Bill Mullins after his final return to Clonmel in the late 1990s"
             className="mt-4"
             width={400}
             height={24}
           />
-          <p>Whilst these pleas fell upon deaf ears, William passed away in the workhouse in Clonmel at the age of 67 from a bladder infection.  Bridget outlived Wililam.  However, it is known when she died.</p>
         </ContentPage>
     </>;
   }
