4287f406c7561fcb3c3c792e2423fc2268e1f49f
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-21 16:53:02 +0100

    Lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
